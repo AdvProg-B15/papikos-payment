5fa9ff548db46f31016f2f0ee70d23a0ce52d3d8
Adiieeee <adiansyah.nugraha@gmail.com>
2025-04-11 15:42:20 +0700

    first commit

---
+++ src/test/java/id/ac/ui/cs/advprog/papikos/payment/PaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.papikos.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
