a5294c7e006c62a9cb26d12dd74462c0615ecef6
Adiieeee <adiansyah.nugraha@gmail.com>
2025-04-11 16:22:45 +0700

    [RED] Add test for PaymentController

---
+++ src/test/java/id/ac/ui/cs/advprog/papikos/payment/controller/PaymentController.java
@@ -0,0 +1,17 @@
+package id.ac.ui.cs.advprog.papikos.payment.controller;
+
+import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
+import id.ac.ui.cs.advprog.papikos.payment.service.PaymentService;
+
+class PaymentControllerTest {
+    @Test
+    void testTopUpEndpoint() {
+        PaymentService service = mock(PaymentService.class);
+        PaymentController controller = new PaymentController(service);
+
+        ResponseEntity<?> response = controller.topUp("user1", 100000);
+        assertEquals(200, response.getStatusCode().value());
+    }
+}
