c4a549eb73b1eec7edb1a82bd18b0a6ee0dfa84c
Adiieeee <adiansyah.nugraha@gmail.com>
2025-04-11 16:10:10 +0700

    [RED] Add test for BalanceRepository

---
+++ src/test/java/id/ac/ui/cs/advprog/papikos/payment/repository/BalanceRepositoryTest.java
@@ -0,0 +1,24 @@
+package id.ac.ui.cs.advprog.papikos.payment.repository;
+
+import id.ac.ui.cs.advprog.payment.model.Balance;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class BalanceRepositoryTest {
+    @Autowired
+    private BalanceRepository balanceRepository;
+
+    @Test
+    void testFindByUserId() {
+        Balance balance = new Balance();
+        balance.setUserId("user1");
+        balanceRepository.save(balance);
+
+        Optional<Balance> found = balanceRepository.findByUserId("user1");
+        assertTrue(found.isPresent());
+    }
+}
