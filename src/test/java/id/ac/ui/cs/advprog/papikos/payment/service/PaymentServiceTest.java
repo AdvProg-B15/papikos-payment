51daa83cf8d9db630a09b59837b635435a0659c2
Adiieeee <adiansyah.nugraha@gmail.com>
2025-04-11 16:19:51 +0700

    [RED] Add test for PaymentService

---
+++ src/test/java/id/ac/ui/cs/advprog/papikos/payment/service/PaymentServiceTest.java
@@ -0,0 +1,17 @@
+package id.ac.ui.cs.advprog.papikos.payment.service;
+
+import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.Test;
+import id.ac.ui.cs.advprog.payment.commands.PaymentCommand;
+import id.ac.ui.cs.advprog.payment.repository.BalanceRepository;
+
+class PaymentServiceTest {
+    @Test
+    void testTopUp() {
+        BalanceRepository balanceRepo = mock(BalanceRepository.class);
+        PaymentService paymentService = new PaymentService(balanceRepo, null);
+
+        paymentService.topUp("user1", 100000);
+        verify(balanceRepo, atLeastOnce()).findByUserId("user1");
+    }
+}
